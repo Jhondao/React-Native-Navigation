7020670494a7bd6c2356cd4b5549e8b2df28b90c
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2022-12-09 21:33:20 -0500

    [ADD] ENviando argumentos entre pantallas

--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, TouchableOpacity } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { styles } from '../theme/appTheme';
+import { PersonaScreen } from './PersonaScreen';
 
 
 
@@ -16,6 +17,24 @@ export const Pagina1Screen = ({ navigation }: Props) => {
           title='Ir página 2'
           onPress={() => navigation.navigate('Pagina2Screen')}
         />
+
+        {/* <Button 
+          title='Ir persona'
+          onPress={() => navigation.navigate('PersonaScreen') }
+        /> */}
+
+        <Text>Navegar con argumentos</Text>
+
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity style={{...styles.botonGrande, backgroundColor:'#5856D6'}} onPress={() => navigation.navigate('PersonaScreen', {id:1, nombre:'Pedro'})}>
+            <Text style={styles.botonGrandeTexto}> Pedro </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{...styles.botonGrande, backgroundColor:'#FF9427'}} onPress={() => navigation.navigate('PersonaScreen', {id:2, nombre:'María'})}>
+            <Text style={styles.botonGrandeTexto}> María </Text>
+          </TouchableOpacity>
+        </View>
+
     </View>
   )
 }
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -9,5 +9,20 @@ export const styles = StyleSheet.create({
     title:{
         fontSize: 30,
         marginBottom: 15
+    },
+    botonGrande:{
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 10
+    },
+
+    botonGrandeTexto:{
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
     }
 });
