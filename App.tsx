fc6e6605b858d00f51480df7bda26a725aa7f255
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2022-12-10 16:07:07 -0500

    [ADD] Menu lateral con Drawer

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import 'react-native-gesture-handler';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 export const App = () => {
   return (
     <NavigationContainer> 
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <MenuLateralBasico />
     </NavigationContainer>
   )
 }
