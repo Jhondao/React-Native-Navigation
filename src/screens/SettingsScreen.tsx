edc8fd49c88a23415c998f1d1347ca24f83b6cf3
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2023-01-17 21:41:57 -0500

    [ADD] Consumiendo el context para el sitio de settings

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, SafeAreaView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const SettingsScreen = () => {
 
   const insets = useSafeAreaInsets();
+
+  const { authState } = useContext(AuthContext);
   
   return (
     <View style={{...styles.globalMargin, marginTop: insets.top + 20}}>
         <Text style={styles.title}>SettingsScreen</Text>
+        <Text> {JSON.stringify(authState, null, 4)} </Text>
     </View>
   )
 }
