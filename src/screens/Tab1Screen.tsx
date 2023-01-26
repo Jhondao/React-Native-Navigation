db66fdee8672e7017320f130e1cf0e6f81bab175
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2023-01-25 21:21:01 -0500

    [ADD ] TouchableIcon - Custom Component

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { View, Text } from 'react-native';
-import { styles } from '../theme/appTheme';
+import { styles, colores } from '../theme/appTheme';
+import { TouchableIcon } from '../components/TouchableIcon';
 
 
 export const Tab1Screen = () => {
@@ -15,6 +16,17 @@ export const Tab1Screen = () => {
   return (
     <View style={ styles.globalMargin}>
         <Text style={ styles.title }> Iconos </Text>
+        
+        <Text> 
+            <TouchableIcon iconName='attach-outline' ></TouchableIcon>  
+            <TouchableIcon iconName='airplane-outline'></TouchableIcon>  
+            <TouchableIcon iconName='bonfire-outline' ></TouchableIcon>  
+            <TouchableIcon iconName='calculator-outline'></TouchableIcon>
+            <TouchableIcon iconName='chatbubble-ellipses-outline' ></TouchableIcon>
+            <TouchableIcon iconName='images-outline' ></TouchableIcon>
+            <TouchableIcon iconName='leaf-outline' ></TouchableIcon>
+        </Text>
+
     </View>
   )
 }
